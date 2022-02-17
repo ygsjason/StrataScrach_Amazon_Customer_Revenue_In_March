ed890c39148a98fb05eaa6aefd27d3a5ce8acc46
ygsjason <99283954+ygsjason@users.noreply.github.com>
2022-02-17 14:18:51 -0600

    Update Amazon_Customer_Revenue_In_March.py

--- Amazon_Customer_Revenue_In_March.py
+++ Amazon_Customer_Revenue_In_March.py
@@ -8,6 +8,8 @@ df1 = orders.drop_duplicates()
 df2 = df1[(df1.order_date >= '2019-03-01') & (df1.order_date <= '2019-03-31')]
 df2 = df1[df1.order_date.between('2019-03-01', '2019-03-31')]
 df2 = df1[df1.order_date.dt.strftime("%Y-%b") == "2019-Mar"]
+# if you don't know how many days in March
+df2 = df1[(df1.order_date > '2019-03') & (df1.order_date < '2019-04')]
 
 # groupby cust_id to calculate the total cost
 df2.groupby('cust_id')['total_order_cost'].sum().reset_index(name = "revenue").sort_values('revenue', ascending = [False])
